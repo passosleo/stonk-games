7a7fe61014e4f809af76a76d63d1fd8dfe44c3f8
leopassos <leonardopassos@sou.faccat.br>
2022-02-22 22:52:22 -0300

    feat: header

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -1,14 +1,79 @@
 import React from "react";
-import { Box, Button } from "@chakra-ui/react";
+import { Box, Button, Divider, Flex, Heading, Input, InputGroup, InputLeftAddon, InputRightAddon, Icon, HStack, Text, Image } from "@chakra-ui/react";
+import { AiOutlineSearch, AiOutlineLogout } from 'react-icons/ai'
+import { IoExitOutline, IoPersonOutline } from 'react-icons/io5'
 import { Link } from "react-router-dom";
+import Logo from "../assets/images/logo.png"
 
 const Layout = ({ children }) => {
   return (
-    <Box>
-      <Button bg="cyan.200">
-        <Link to="/">Home</Link>
-      </Button>
-      {children}
+    <Box color='gray.50'>
+      <Flex
+        bg='gray.900'
+        h={22}
+        position='fixed'
+        top={0}
+        left={0}
+        right={0}
+        zIndex={1}
+        borderBottomWidth={1}
+        borderColor='purple.900'
+        justifyContent='center'
+      >
+        <Flex
+          w='100%'
+          alignItems='center'
+          justifyContent='space-between'
+          px='5%'
+          maxWidth='1440px'
+        >
+          <Image w={70} src={Logo} />
+
+          <Text>Games</Text>
+
+          <Text>Favorites</Text>
+
+          <InputGroup
+            size='sm'
+            w={80}
+            borderColor='purple.900'
+            _onFocus={{
+              outline: 'none',
+              boxShadow: 'none',
+            }}
+          >
+            <Input
+              placeholder='Pesquisar'
+              bg='gray.50'
+              color='gray.900'
+              borderRadius={8}
+            />
+            <InputRightAddon
+              borderRadius={8}
+              bg='purple.900'
+              children={
+                <Icon as={AiOutlineSearch} w={5} h={5} color='gray.50' />
+              }
+            />
+          </InputGroup>
+
+          <HStack spacing={5} h='100%'>
+            <HStack>
+              <Text>Profile</Text>
+              <Icon as={IoPersonOutline} w={6} h={6} color='gray.50' />
+            </HStack>
+            <Divider orientation='vertical' h='40%' />
+            <HStack>
+              <Text>Logout</Text>
+              <Icon as={IoExitOutline} w={7} h={7} color='gray.50' />
+            </HStack>
+          </HStack>
+        </Flex>
+
+      </Flex>
+      <Box bg='gray.800' mt={20} p={10}>
+        {children}
+      </Box>
     </Box>
   );
 };
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,10 +1,52 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
+import { Box, Flex, Image, Link, Text } from "@chakra-ui/react";
 
 const Home = () => {
+  const [games, setGames] = useState([]);
+
+  useEffect(() => {
+    Axios.get("https://www.freetogame.com/api/games?platform=pc").then(
+      (response) => {
+        setGames(response.data.slice(0, 18))
+      }
+    );
+  }, [])
+  console.log(games)
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <Flex direction='row' flexWrap='wrap' justifyContent='center'>
+      
+      {games.map((game, index) =>
+        <Flex
+          key={index}
+          direction='column'
+          p={5}
+          transition='350ms'
+          _hover={{
+            transition: '350ms',
+            transform: 'scale(1.07)'
+          }}
+        >
+          <Link
+            href={game.game_url}
+            _active={{
+              boxShadow: 'none'
+            }}
+            _hover={{
+              textDecoration: 'none'
+            }}>
+            <Image w={80} borderRadius={10} src={game.thumbnail}></Image>
+            <Text fontWeight='bold' my={1}>{game.title}</Text>
+            <Text maxWidth={80}>
+              {game.short_description.length > 100
+                ? game.short_description.substring(0, 100) + '...'
+                : game.short_description}
+            </Text>
+          </Link>
+        </Flex>
+      )}
+
+    </Flex>
   );
 };
 
