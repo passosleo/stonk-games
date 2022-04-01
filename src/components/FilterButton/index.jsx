89d3b2d4da78c9cdcc4987f31a86530824353bf6
Aluno Faccat <aluno@lab203-10.LABORATORIO>
2022-03-31 22:02:09 -0300

    feat: filter component

--- src/components/FilterButton/index.jsx
+++ src/components/FilterButton/index.jsx
@@ -1,15 +1,15 @@
 import React, { useState } from "react";
 import { Box, Flex, Link, Image, Button, Icon, Text, HStack, Menu, MenuButton, MenuList, MenuItem } from "@chakra-ui/react";
 import { IoLogoWindows } from 'react-icons/io5';
-import { AiOutlineSortAscending } from 'react-icons/ai';
 import { BiWorld } from 'react-icons/bi';
 
-const FilterButton = ({ setFilter }) => {
+const FilterButton = ({ title, icon, options, setFilter }) => {
+  console.log(options)
   return (
     <Menu>
       <MenuButton
         as={Button}
-        rightIcon={<Icon as={AiOutlineSortAscending} w={6} h={6} />}
+        rightIcon={<Icon as={icon} w={6} h={6} />}
         backgroundColor="gray.900"
         borderWidth={1}
         borderColor='purple.900'
@@ -21,37 +21,19 @@ const FilterButton = ({ setFilter }) => {
           backgroundColor: "purple.900"
         }}
       >
-        Filter
+        {title}
       </MenuButton>
       <MenuList borderColor='purple.900' bg='gray.700'>
-        <MenuItem
-          _hover={{ background: 'purple.900' }}
-          _focus={{ background: 'purple.900' }}
-          onClick={() => setFilter("alphabetical")}
-        >
-          Alphabetical
-        </MenuItem>
-        <MenuItem
-          _hover={{ background: 'purple.900' }}
-          _focus={{ background: 'purple.900' }}
-          onClick={() => setFilter("popularity")}
-        >
-          Popularity
-        </MenuItem>
-        <MenuItem
-          _hover={{ background: 'purple.900' }}
-          _focus={{ background: 'purple.900' }}
-          onClick={() => setFilter("release-date")}
-        >
-          Release Date
-        </MenuItem>
-        <MenuItem
-          _hover={{ background: 'purple.900' }}
-          _focus={{ background: 'purple.900' }}
-          onClick={() => setFilter("relevance")}
-        >
-          Relevance
-        </MenuItem>
+        {options.map((option, index) => (
+          <MenuItem
+            key={option.id || index}
+            _hover={{ background: 'purple.900' }}
+            _focus={{ background: 'purple.900' }}
+            onClick={() => setFilter(option.label.toLowerCase())}
+          >
+            {option.label}
+          </MenuItem>
+        ))}
       </MenuList>
     </Menu>
   );
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,36 +1,37 @@
 import React, { useState, useEffect } from "react";
 import Axios from "axios";
 import { Box, Flex } from "@chakra-ui/react";
+import { AiOutlineSortAscending } from 'react-icons/ai';
 import GameCard from "../../components/GameCard";
 import FilterButton from "../../components/FilterButton";
 
 const Home = () => {
   const [games, setGames] = useState([]);
   const [filter, setFilter] = useState("");
-  const [chande, setChange] = useState("");
-  // console.log(filter);
 
   useEffect(() => {
-    Axios.get("https://www.freetogame.com/api/games?platform=all&sort-by=popularity").then(
-      (response) => {
-        setGames(response.data.slice(14, 32))
-      }
-    );
-  }, [])
-
-  useEffect(() => {
-    Axios.get(`https://www.freetogame.com/api/games?platform=all&sort-by=${filter}`).then(
+    Axios.get(`https://www.freetogame.com/api/games?platform=all&sort-by=${filter ? filter : "popularity"} `).then(
       (response) => {
         setGames(response.data.slice(14, 32))
       }
     );
   }, [filter])
 
-
   return (
     <Box>
       <Flex>
-        <FilterButton setFilter={setFilter} />
+        <FilterButton
+          title="Filter"
+          onHeader={false}
+          icon={AiOutlineSortAscending}
+          setFilter={setFilter}
+          options={[
+            {id: 1, label: "Alphabetical"},
+            {id: 2, label: "Popularity"},
+            {id: 3, label: "Release-Date"},
+            {id: 4, label: "Relevant"}
+          ]}
+        />
       </Flex>
       <Flex
         flexWrap='wrap'
