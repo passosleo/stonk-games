8431cad8f7850c30d45ce417c0346ae1e7337843
Aluno Faccat <aluno@lab203-28.LABORATORIO>
2022-04-05 21:33:15 -0300

    fix: static data

---
+++ src/components/CategorieTag/index.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import { Box, Flex, Link, Image, Button, Icon, Text, HStack, Menu, MenuButton, MenuList, MenuItem } from "@chakra-ui/react";
+
+const CategorieTag = () => {
+  return (
+    <HStack>
+
+    </HStack>
+  );
+};
+
+export default CategorieTag;
